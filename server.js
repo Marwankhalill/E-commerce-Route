a96786c9ae1cbb0b81f1aa2b7244996c3037eb35
Marwan Khalil <marwanmedo7777@gmail.com>
2026-07-05 00:43:42 +0300

    Stripe added

--- server.js
+++ server.js
@@ -7,12 +7,32 @@ import { bootstrap } from "./src/modules/bootstrap.js";
 import { AppError } from "./src/utils/AppError.js";
 import { globalError } from "./src/middlewares/globalError.js";
 import cors from "cors";
+import { asyncHandler } from "./src/utils/asyncHandler.js";
 
 const app = express();
 const port = process.env.port;
 app.use(cors());
 
 dbConnection();
+
+app.post(
+  "/api/webhook",
+  express.raw({ type: "application/json" }),
+  asyncHandler((req, res) => {
+    const sig = req.headers["stripe-signature"];
+    let event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      whsec_fMI3ZebpjkvCQsRPovBmjsO2nulPGZqB,
+    );
+    let checkout;
+    if (event.type == "checkout.session.completed") {
+      checkout = event.data.object;
+    }
+
+    res.status(200).json({ message: "success", checkout });
+  }),
+);
 
 app.set("query parser", "extended");
 app.use(express.json());
